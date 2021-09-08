3e69084dd8872dee66fc80e2c0e458c37f3a076f
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-09-08 19:01:14 +0200

    зробив щоб працювала жива сторінка

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import './App.css';
 import Register from './pages/register';
 import Login from './pages/login';
 import ContactsPage from './pages/contacts';
